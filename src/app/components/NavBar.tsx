0282e3d93f12b9b57d4da05dd95fde376b891934
Vivian <vivian.ren@students.mq.edu.au>
2024-11-01 12:56:03 +1100

    Changed Username

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -7,8 +7,8 @@ const Navbar = () => {
       {/* ICONS AND USER */}
       <div className='flex items-center gap-6 justify-end w-full'>
         <div className='flex flex-col'>
-          <span className="text-xs leading-3 font-medium">Darrell Lim</span>
-          <span className="text-[10px] text-gray-500 text-right">Admin</span>
+          <span className="text-xs leading-3 font-medium">Espana</span>
+          <span className="text-[10px] text-gray-500 text-right">Convenor</span>
         </div>
         <Image src="/avatar.png" alt="" width={36} height={36} className="rounded-full"/>
       </div>
